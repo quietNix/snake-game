853faef1331c74e5b3ad8eab91e08fc6583670d1
Nikhil Kumar <42882230+quietNix@users.noreply.github.com>
2022-02-01 05:22:48 +0530

    Fixed Bug of reverse movement

--- input.js
+++ input.js
@@ -1,6 +1,6 @@
 let inputDirection = { x: 0, y: 0 }
 let lastInputDirection = { x: 0, y: 0 }
-import {getSnakeHead} from "./snake.js";
+import { getSnakeHead } from "./snake.js";
 
 window.addEventListener('keydown', e => {
   switch (e.key) {
@@ -23,25 +23,25 @@ window.addEventListener('keydown', e => {
   }
 })
 
-window.addEventListener("click", e=>{
-  
-  let small = (window.innerHeight>window.innerWidth)?window.innerWidth:window.innerHeight;
+window.addEventListener("click", e => {
+
+  let small = (window.innerHeight > window.innerWidth) ? window.innerWidth : window.innerHeight;
   let long = (window.innerHeight < window.innerWidth) ? window.innerWidth : window.innerHeight;
-  let extra = (long-small)/2;
-  
-  let diff = (window.innerHeight < window.innerWidth) ? {x:extra, y:0}: {x:0, y:extra}
+  let extra = (long - small) / 2;
+
+  let diff = (window.innerHeight < window.innerWidth) ? { x: extra, y: 0 } : { x: 0, y: extra }
   let head = getSnakeHead();
 
   let y = e.clientY - (diff.y + head.y * small / 21);
   let x = e.clientX - (diff.x + head.x * small / 21);
 
-  if(Math.abs(x)>Math.abs(y)){
-    if(x>0)inputDirection={x:1, y:0}
-    else inputDirection={x:-1, y:0}
+  if (Math.abs(x) > Math.abs(y)) {
+    if (x > 0 && lastInputDirection.x === 0) inputDirection = { x: 1, y: 0 }
+    else if (lastInputDirection.x === 0) inputDirection = { x: -1, y: 0 }
   }
-  else{
-    if(y>0)inputDirection={x:0, y:1}
-    else inputDirection={x:0, y:-1}
+  else {
+    if (y > 0 && lastInputDirection.y === 0) inputDirection = { x: 0, y: 1 }
+    else if (lastInputDirection.y === 0) inputDirection = { x: 0, y: -1 }
   }
 })
 
